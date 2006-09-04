342ace04d55168072a105efaa2ec8aacc18f4e16
fourmond <fourmond@f6f669a6-2b79-499e-8f05-be2b991fc3d0>
2006-09-04 19:51:44 +0000

    Really add Function/lib/Function_extras.rb to the repository
    git-svn-id: svn+ssh://rubyforge.org/var/svn/tioga/trunk/tioga@157 f6f669a6-2b79-499e-8f05-be2b991fc3d0

---
+++ split/Function/lib/Function_extras.rb
@@ -0,0 +1,33 @@
+#    Function_extras.rb
+# 
+#    Some extra functions that are much more compact and hardly any slower in
+#    pure Ruby
+# 
+#    Copyright (C) 2006  Vincent Fourmond
+# 
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Library Public License as published
+#    by the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Library General Public License for more details.
+# 
+#    You should have received a copy of the GNU Library General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+   
+
+module Dobjects
+  class Function
+
+    # Returns [xmin, ymin, xmax, ymax]
+    def bounds
+      xmin,xmax = x.bounds
+      ymin,ymax = y.bounds
+      return [xmin, ymin, xmax, ymax]
+    end
+  end
+end
