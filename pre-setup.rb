1bdf4a35012390ec4bcc2cf7d0dba7d55a0cf508
fourmond <fourmond@f6f669a6-2b79-499e-8f05-be2b991fc3d0>
2010-09-13 23:09:55 +0000

    [tioga] Installing MacOS-specific files
    git-svn-id: svn+ssh://rubyforge.org/var/svn/tioga/trunk/tioga@578 f6f669a6-2b79-499e-8f05-be2b991fc3d0

--- pre-setup.rb
+++ pre-setup.rb
@@ -1,2 +1,15 @@
-# Generate include files and preambles
+# We do two things here:
+require 'ftools'
+
+# First, generate include files and preambles
 system "#{config("rubyprog")} misc/mk_tioga_sty.rb"
+
+if Config::CONFIG["target"] =~ /darwin/i
+  File.open("bin/repreview", 'w') do |f|
+    f.puts '#!/bin/sh'
+    f.puts "osascript #{config('bindir')}/Reload_Preview_Document.scpt $*"
+  end
+  File.copy("misc/Reload_Preview_Document.scpt", "bin")
+else
+  puts "Skipping MacOS-specific files"
+end
