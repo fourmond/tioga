5e4e734406e14bb211eed987b88dd8dfc37c7905
Francois Hebert <fmh33@cornell.edu>
2015-09-10 15:21:57 -0400

    Update pre-setup.rb to use RbConfig
    Use of deprecated Config was giving a warning.

--- pre-setup.rb
+++ pre-setup.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 # First, generate include files and preambles
 system "#{config("rubyprog")} misc/mk_tioga_sty.rb"
 
-if Config::CONFIG["target"] =~ /darwin/i
+if RbConfig::CONFIG["target"] =~ /darwin/i
   File.open("bin/repreview", 'w') do |f|
     f.puts '#!/bin/sh'
     f.puts "osascript #{config('bindir')}/Reload_Preview_Document.scpt $*"
